b0c41061f01e96516fe509272cf6de97ace5cfe9
Mehmet Akgul <mehmetakgul.dev@gmail.com>
2023-07-25 14:48:44 +0300

    Added hashCode and toString methods for Book And Author

--- src/main/java/mak/sprigframework/spring6webapp/domain/Book.java
+++ src/main/java/mak/sprigframework/spring6webapp/domain/Book.java
@@ -51,4 +51,28 @@ public class Book {
         this.isbn = isbn;
     }
 
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        return id == book.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (id ^ (id >>> 32));
+    }
 }
